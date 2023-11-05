b9424fa0c347969708e66b8df72d9e6ee1373edc
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-11-05 13:19:09 +0100

    pagina informacion

--- src/app/components/info-film/info-film.component.ts
+++ src/app/components/info-film/info-film.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 import { Filmroot } from 'src/app/model/film-detail-interface';
 import { FilmService } from 'src/app/service/film.service';
 
@@ -11,19 +12,19 @@ import { FilmService } from 'src/app/service/film.service';
 export class InfoFilmComponent implements OnInit {
 
   id!: number;
-  route: ActivatedRoute = Inject(ActivatedRoute);
   film!: Filmroot;
 
-  constructor(private filmDetail: FilmService) {
-    this.id = this.route.snapshot.params['id'];
-
-
-  }
+  constructor(private route: ActivatedRoute, private filmDetail: FilmService) { }
 
   ngOnInit(): void {
+    this.id = Number(this.route.snapshot.params['id']);
     this.filmDetail.findById(this.id).subscribe(resp => {
       this.film = resp;
-    })
+    });
+  }
+
+  getImg() {
+    return `https://www.themoviedb.org/t/p/w220_and_h330_face${this.film.poster_path}`;
   }
 
 }
